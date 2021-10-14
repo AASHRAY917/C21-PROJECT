1045a1a883c0efd400043cc4f83e2fcf79f0742b
AaSh <84702164+AASHRAY917@users.noreply.github.com>
2021-10-14 16:04:54 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball
+{
+  constructor(x, y, radius) 
+  {
+    let options = 
+    {
+      isStatic:false,
+      'restitution':0.3,
+      'friction':0.5,
+      'density':1.2
+    };
+    
+    this.body = Bodies.circle(x, y, radius, options);
+    this.radius = radius;
+    World.add(world, this.body);
+  }
+
+  show() 
+  {
+    let pos = this.body.position;
+    
+    
+    ellipseMode(RADIUS);
+    stroke(255);
+    circle(pos.x, pos.y, this.radius);
+    
+  }
+}
